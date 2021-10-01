8fc07762f2490b526728219e69c10c3f52ed965a
FRM <75284956+FRM-Developer@users.noreply.github.com>
2021-10-01 13:02:50 +0700

    update index.ts

---
+++ index.ts
@@ -0,0 +1,29 @@
+import makeWASocket from '@adiwajshing/baileys-md'
+
+async function connectToWhatsApp () {
+    const conn = makeWASocket({
+        // can provide additional config here
+        printQRInTerminal: true
+    })
+    sock.ev.on('connection.update', (update) => {
+        const { connection, lastDisconnect } = update
+        if(connection === 'close') {
+            const shouldReconnect = (lastDisconnect.error as Boom)?.output?.statusCode !== DisconnectReason.loggedOut
+            console.log('connection closed due to ', lastDisconnect.error, ', reconnecting ', shouldReconnect)
+            // reconnect if not logged out
+            if(shouldReconnect) {
+                sock = startSock()
+            }
+        } else if(connection === 'open') {
+            console.log('opened connection')
+        }
+    })
+    sock.ev.on('messages.upsert', m => {
+        console.log(JSON.stringify(m, undefined, 2))
+
+        console.log('replying to', m.messages[0].key.remoteJid)
+        sendMessageWTyping({ text: 'Hello there!' }, m.messages[0].key.remoteJid!)
+    })
+}
+// run in main file
+connectToWhatsApp()
