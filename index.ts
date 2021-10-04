ad6c27d54e721e47296aaca2a19cf8599066ea7c
FRM <75284956+FRM-Developer@users.noreply.github.com>
2021-10-04 07:55:20 +0700

    update index.ts

--- index.ts
+++ index.ts
@@ -5,17 +5,16 @@ async function connectToWhatsApp () {
         // can provide additional config here
         printQRInTerminal: true
     })
-    sock.ev.on('connection.update', (update) => {
+    conn.ev.on('connection.update', (update) => {
         const { connection, lastDisconnect } = update
         if(connection === 'open') {
             console.log('opened connection')
         }
     })
-    sock.ev.on('messages.upsert', m => {
+    conn.ev.on('messages.upsert', m => {
         console.log(JSON.stringify(m, undefined, 2))
 
         console.log('replying to', m.messages[0].key.remoteJid)
-        sendMessageWTyping({ text: 'Hello there!' }, m.messages[0].key.remoteJid)
     })
 }
 // run in main file
